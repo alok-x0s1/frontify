80b33e6e6cc5f511adf032824f4a9315e81e7f91
Alok Yadav <yalok6321@gmail.com>
2024-05-09 11:43:34 +0530

    Syntax debugging

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
-const { configureStore } = require("@reduxjs/toolkit");
+import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "../features/authSlice"
 
 const store = configureStore({
     reducer: {
         // All reducers
+        auth: authReducer
     }
 })
 
