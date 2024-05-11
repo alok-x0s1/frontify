4755675a28fd2a7698522f0e43af78ce08c80f4e
Alok Yadav <yalok6321@gmail.com>
2024-05-11 16:24:10 +0530

    Made components for Login, Signup, and Postcard

---
+++ src/components/authLayout.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected ({children, authentication = true}) {
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatues = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        //TODO: more easy
+
+        // if(authStatues === true) {
+        //     navigate("/")
+        // } else if(authStatues === false) {
+        //     navigate("/login")
+        // }
+        // let authValue = authStatues === true ? "true" : "false";
+
+        if(authentication && authStatues !== authentication) {
+            navigate('/login')
+        } else if(!authentication && authStatues !== authentication) {
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authentication, navigate, authStatues])
+
+  return (
+    <div>authLayout</div>
+  )
+}
