e732c144eed3f516d0693ba2f608f5d0d053a321
Alok Yadav <yalok6321@gmail.com>
2024-05-13 10:42:19 +0530

    Debugging

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,13 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from "./RTE"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import PostForm from "./post-form/PostForm"
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
 
 export {
     Header,
@@ -11,5 +18,12 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Button,
+    Input
 }
